41b83962e8de9a7b3cc3e42a710fd423003c0384
mei <j6xul6ing@gmail.com>
2021-07-10 19:08:01 +0800

    forget to save hahaha

--- app_test.py
+++ app_test.py
@@ -28,8 +28,9 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('eeca7lo2Ebs14wFbm4AXhvU/5qj569ywDfMxQ9a4cZaIqDKE4TFiHNNWvUaah2A2clVoV9McprdK6K/guNEZiSV8P6+HRgPr2Z3mB+3it2r3q2IDUJByKbPMoGwTrduDjjXZiW5xAp2FWQzSC0Tc7wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('3b99db8557a3bb97f24e626b0d84837c')
 
+from MessageRouter import MessageRouter
 from controllers.EatIntroduceController import EatIntroduceController
-
+postbackRouter = MessageRouter('postback')
 postbackRouter.add('/keto', EatIntroduceController.keto)
 postbackRouter.add('/ketoA', EatIntroduceController.ketoA)
 postbackRouter.add('/ketoB', EatIntroduceController.ketoB)
