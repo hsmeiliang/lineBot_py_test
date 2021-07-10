485c884db8634b9cc6bfc32076db1c7e6c7a911b
mei <j6xul6ing@gmail.com>
2021-07-10 20:20:58 +0800

    add name

--- MessageRouter.py
+++ MessageRouter.py
@@ -5,18 +5,15 @@ class MessageRouter():
         self._messageRouterMapping = {}
         self._routerType = routerType
 
-    @staticmethod
     def findFuncRegex(self, message):
         for regPattern in self._messageRouterMapping:
             if re.match(regPattern, message):
                 return self._messageRouterMapping[regPattern]
         return None
 
-    @staticmethod
     def add(self, message, func):
         self._messageRouterMapping[message] = func
 
-    @staticmethod
     def route(self, event):
         if (self._routerType == 'text'):
             func = self.findFuncRegex(event.message.text)
