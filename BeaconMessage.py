3577e198f3b2e4d3e2230cdde6179c88ecfda0f9
mei <j6xul6ing@gmail.com>
2021-07-12 21:39:14 +0800

    debug

--- BeaconMessage.py
+++ BeaconMessage.py
@@ -1,8 +1,4 @@
-from linebot.models import FlexSendMessage
-from linebot.models.flex_message import (
-    BubbleContainer, ImageComponent, TextComponent, BoxComponent
-)
-from linebot.models.actions import URIAction
+from linebot.models import *
 
 from linebot import (LineBotApi, WebhookHandler)
 line_bot_api = LineBotApi('eeca7lo2Ebs14wFbm4AXhvU/5qj569ywDfMxQ9a4cZaIqDKE4TFiHNNWvUaah2A2clVoV9McprdK6K/guNEZiSV8P6+HRgPr2Z3mB+3it2r3q2IDUJByKbPMoGwTrduDjjXZiW5xAp2FWQzSC0Tc7wdB04t89/1O/w1cDnyilFU=')
@@ -15,30 +11,30 @@ class BeaconMessage():
         pass
 
     def buildComponent(self):
-        return FlexSendMessage.BubbleContainer(
+        return BubbleContainer(
             direction = 'ltr',
-            body = FlexSendMessage.BoxComponent(
+            body = BoxComponent(
                 layout = 'vertical',
                 contents = [BoxComponent(
                     layout = 'vertical',
                     contents = [
-                        FlexSendMessage.TextComponent(text='附近飲食推薦', weight='bold', size='xl'),
-                        FlexSendMessage.TextComponent(text='以下是為你推薦附近的午餐菜單', weight='bold', size='xs', color='#555555'),
-                        FlexSendMessage.TextComponent(text='點擊即可新增飲食紀錄', weight='bold', size='xs', color='#555555')
+                        TextComponent(text='附近飲食推薦', weight='bold', size='xl'),
+                        TextComponent(text='以下是為你推薦附近的午餐菜單', weight='bold', size='xs', color='#555555'),
+                        TextComponent(text='點擊即可新增飲食紀錄', weight='bold', size='xs', color='#555555')
                     ]
                 ),
-                FlexSendMessage.BoxComponent(
+                BoxComponent(
                     layout='vertical',spacing='xs',margin='xl',
                     contents=[
-                        FlexSendMessage.BoxComponent(
+                        BoxComponent(
                             layout='horizontal',
                             contents=[
-                                FlexSendMessage.TextComponent(text='皿富器食 minfood 花雕野菜', weight='bold', size='md', color='#555555', flex=0),
-                                FlexSendMessage.TextComponent(text='$' + str(255), weight='bold', size='md', color='#111111', align='end')
+                                TextComponent(text='皿富器食 minfood 花雕野菜', weight='bold', size='md', color='#555555', flex=0),
+                                TextComponent(text='$' + str(255), weight='bold', size='md', color='#111111', align='end')
                             ]
                         ),
-                        FlexSendMessage.TextComponent(text='餐點熱量為' + str(340) + '大卡', weight='bold', size='xs', color='#555555'),
-                        FlexSendMessage.ImageComponent(url='https://i.imgur.com/376iFbj.jpg', margin='none',align='center',size='4xl')
+                        TextComponent(text='餐點熱量為' + str(340) + '大卡', weight='bold', size='xs', color='#555555'),
+                        ImageComponent(url='https://i.imgur.com/376iFbj.jpg', margin='none',align='center',size='4xl')
                     ]
                 )
                 ]
