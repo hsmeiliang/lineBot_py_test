a3286479147b562bb29d8daa3828ae74a02d47aa
mei <j6xul6ing@gmail.com>
2021-07-15 20:22:37 +0800

    debug

--- BeaconMessage.py
+++ BeaconMessage.py
@@ -57,10 +57,10 @@ class BeaconMessage():
                         TextComponent(text=item['mealName'], weight='bold', size='md', color='#555555', flex=0),
                         TextComponent(text='$' + str(item['price']), weight='bold', size='md', color='#111111', align='end')
                     ]
-                ),
-                TextComponent(text='餐點熱量為' + str(item['kcal']) + '大卡', weight='bold', size='xs', color='#555555'),
-                ImageComponent(url=item['picture'], margin='none',align='center',size='4xl'),
+                )
             )
+            comp.append(TextComponent(text='餐點熱量為' + str(item['kcal']) + '大卡', weight='bold', size='xs', color='#555555'))
+            comp.append(ImageComponent(url=item['picture'], margin='none',align='center',size='4xl'))
 
         bubble = BubbleContainer(
             direction = 'ltr',
@@ -69,9 +69,7 @@ class BeaconMessage():
                 contents = [BoxComponent(
                     layout = 'vertical',
                     contents = [
-                        TextComponent(text='附近飲食推薦', weight='bold', size='xl'),
-                        TextComponent(text='以下是為你推薦附近的午餐菜單', weight='bold', size='xs', color='#555555'),
-                        TextComponent(text='點擊即可新增飲食紀錄', weight='bold', size='xs', color='#555555')
+                        TextComponent(text='附近飲食推薦', weight='bold', size='xl')
                     ]
                 ),
                 BoxComponent(
