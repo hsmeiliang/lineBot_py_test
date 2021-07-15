d80fcb7f33f56a173cde7d8bcbd6e384615d00f9
mei <j6xul6ing@gmail.com>
2021-07-15 22:23:52 +0800

    debug

--- BeaconMessage.py
+++ BeaconMessage.py
@@ -48,9 +48,9 @@ class BeaconMessage():
                 BoxComponent(
                     layout='horizontal',
                     contents=[
-                        TextComponent(text=item['start_name'], weight='bold', size='md', color='#111111'),
-                        TextComponent(text=' ' + '往'+' ', weight='bold', size='xs', color='#555555'),
-                        TextComponent(text=item['end_name'], weight='bold', size='md', color='#111111', align='end')
+                        TextComponent(text=item['start_name'], weight='bold', size='md', color='#111111', flex=0),
+                        TextComponent(text=' ' + '往'+' ', weight='bold', size='xs', color='#555555', flex=0),
+                        TextComponent(text=item['end_name'], weight='bold', size='md', color='#111111', flex=0, align='end')
                     ]
                 )
             )
@@ -64,7 +64,7 @@ class BeaconMessage():
                 )
             )
             comp.append(TextComponent(text='路線總長:'+str(item['length'])+'km', weight='bold', size='md', color='#111111', align='end'))
-            comp.append(TextComponent(text=item['web'])
+            comp.append(TextComponent(text=item['web']))
 
         bubble = BubbleContainer(
             direction = 'ltr',
