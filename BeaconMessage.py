b91637cea165e0426abb4cad9b40efbe7ee1e66a
mei <j6xul6ing@gmail.com>
2021-07-15 21:46:03 +0800

    debug

--- BeaconMessage.py
+++ BeaconMessage.py
@@ -65,24 +65,12 @@ class BeaconMessage():
                 contents = [BoxComponent(
                     layout = 'vertical',
                     contents = [
-                        TextComponent(text='附近飲食推薦', weight='bold', size='xl'),
-                        TextComponent(text='以下是為你推薦附近的午餐菜單', weight='bold', size='xs', color='#555555'),
-                        TextComponent(text='點擊即可新增飲食紀錄', weight='bold', size='xs', color='#555555')
+                        TextComponent(text='推薦路線', weight='bold', size='xl')
                     ]
                 ),
                 BoxComponent(
                     layout='vertical',spacing='xs',margin='xl',
-                    contents=[
-                        BoxComponent(
-                            layout='horizontal',
-                            contents=[
-                                TextComponent(text='皿富器食 minfood 花雕野菜', weight='bold', size='md', color='#555555', flex=0),
-                                TextComponent(text='$' + str(255), weight='bold', size='md', color='#111111', align='end')
-                            ]
-                        ),
-                        TextComponent(text='餐點熱量為' + str(340) + '大卡', weight='bold', size='xs', color='#555555'),
-                        ImageComponent(url='https://i.imgur.com/376iFbj.jpg', margin='none',align='center',size='4xl')
-                    ]
+                    contents=comp
                 )
                 ]
             )
