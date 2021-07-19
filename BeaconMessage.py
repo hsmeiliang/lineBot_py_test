d1245dfb1e47d5ddd164f61394db432ed73a4e9c
mei <j6xul6ing@gmail.com>
2021-07-19 20:34:09 +0800

    debug

--- BeaconMessage.py
+++ BeaconMessage.py
@@ -144,48 +144,48 @@ class BeaconMessage():
         Carousel_template = TemplateSendMessage(
             alt_text='Carousel template',
             template=CarouselTemplate(
-            columns=[
-                CarouselColumn(
-                    thumbnail_image_url='顯示在開頭的大圖片網址',
-                    title='this is menu1',
-                    text='description1',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='postback1',
-                            text='postback text1',
-                            data='action=buy&itemid=1'
-                        ),
-                        MessageTemplateAction(
-                            label='message1',
-                            text='message text1'
-                        ),
-                        URITemplateAction(
-                            label='uri1',
-                            uri='http://example.com/1'
-                        )
-                    ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url='顯示在開頭的大圖片網址',
-                    title='this is menu2',
-                    text='description2',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='postback2',
-                            text='postback text2',
-                            data='action=buy&itemid=2'
-                        ),
-                        MessageTemplateAction(
-                            label='message2',
-                            text='message text2'
-                        ),
-                        URITemplateAction(
-                            label='連結2',
-                            uri='http://example.com/2'
-                        )
-                    ]
-                )
-            ]
-        )
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url='顯示在開頭的大圖片網址',
+                        title='this is menu1',
+                        text='description1',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='postback1',
+                                text='postback text1',
+                                data='action=buy&itemid=1'
+                            ),
+                            MessageTemplateAction(
+                                label='message1',
+                                text='message text1'
+                            ),
+                            URITemplateAction(
+                                label='uri1',
+                                uri='http://example.com/1'
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='顯示在開頭的大圖片網址',
+                        title='this is menu2',
+                        text='description2',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='postback2',
+                                text='postback text2',
+                                data='action=buy&itemid=2'
+                            ),
+                            MessageTemplateAction(
+                                label='message2',
+                                text='message text2'
+                            ),
+                            URITemplateAction(
+                                label='連結2',
+                                uri='http://example.com/2'
+                            )
+                        ]
+                    )
+                ]
+            )
         )
         return Carousel_template
