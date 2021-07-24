9946de07d8b42c05d8087f3568d2c20428c81dc4
mei <j6xul6ing@gmail.com>
2021-07-24 21:48:19 +0800

    5

--- GetHealthEdu.py
+++ GetHealthEdu.py
@@ -12,8 +12,8 @@ class GetYoutubeLink():
                     ,'https://www.youtube.com/user/hpagov/videos' # 衛生福利部國民健康署
                     ,'https://www.youtube.com/channel/UCVkryqwFXRc94ZThKMaXdIQ/videos'# 食藥好文網TFDA
                     ,'https://www.youtube.com/c/%E8%A1%9B%E7%94%9F%E7%A6%8F%E5%88%A9%E9%83%A8%E7%A4%BE%E6%9C%83%E5%8F%8A%E5%AE%B6%E5%BA%AD%E7%BD%B2/videos' # 衛生福利部社會及家庭署
-                    ,'https://www.youtube.com/user/myegovnhi/videos' # 衛生福利部中央健康保險署
-                    ,'https://www.youtube.com/channel/UCaHvaC68xsKWT2itI-0utJw/videos' # 衛生福利部【長照專區】
+                    #,'https://www.youtube.com/user/myegovnhi/videos' # 衛生福利部中央健康保險署
+                    #,'https://www.youtube.com/channel/UCaHvaC68xsKWT2itI-0utJw/videos' # 衛生福利部【長照專區】
                     ]
     
     def youtubeLinks(self, url):
