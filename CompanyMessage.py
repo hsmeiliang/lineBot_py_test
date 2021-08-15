cd4f153bce3b27f24736702f2a8e647dc7953a13
mei <j6xul6ing@gmail.com>
2021-08-15 16:35:05 +0800

    test

--- CompanyMessage.py
+++ CompanyMessage.py
@@ -2,6 +2,7 @@ import re
 import requests
 import json
 from linebot.models import *
+import string
 # line_bot_api = LineBotApi('eeca7lo2Ebs14wFbm4AXhvU/5qj569ywDfMxQ9a4cZaIqDKE4TFiHNNWvUaah2A2clVoV9McprdK6K/guNEZiSV8P6+HRgPr2Z3mB+3it2r3q2IDUJByKbPMoGwTrduDjjXZiW5xAp2FWQzSC0Tc7wdB04t89/1O/w1cDnyilFU=')
 # line_bot_api.push_message('Ufbdbbd3db1cc8d560f45d5cd2519b290', TextSendMessage(text = 'push message'))
 # https://cdn4.iconfinder.com/data/icons/coronavirus-color/64/doctor-advise-warning-suggestion-avatar-1024.png
@@ -76,6 +77,12 @@ def HealthRank(user_lineID, result):
         i+=1
     print(comp)
     
+    for i in range(len(result["category"])):
+        if result["category"][i].isupper():
+            title = result["category"][1:i] + ' ' + result["category"][i+1:] + ' '+ 'Ranking'
+
+    title = string.capwords(title)
+
     bubble = BubbleContainer(
         direction = 'ltr',
         body = BoxComponent(
@@ -83,7 +90,7 @@ def HealthRank(user_lineID, result):
             contents = [BoxComponent(
                 layout = 'vertical',
                 contents = [
-                    TextComponent(text=result["category"].capitalize() + "Ranking", weight='bold', size='xl', color='#000000')
+                    TextComponent(text=title, weight='bold', size='xl', color='#000000')
                 ]
             ),
             BoxComponent(
