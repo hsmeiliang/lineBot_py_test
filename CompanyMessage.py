1297b360b7ced0fab02730c2cc9a5555be8f81ea
mei <j6xul6ing@gmail.com>
2021-08-15 16:18:49 +0800

    test

--- CompanyMessage.py
+++ CompanyMessage.py
@@ -83,7 +83,7 @@ def HealthRank(user_lineID, result):
             contents = [BoxComponent(
                 layout = 'vertical',
                 contents = [
-                    TextComponent(text=result["category"].capitalize() + ranking, weight='bold', size='xl', color='#000000')
+                    TextComponent(text=result["category"].capitalize() + "Ranking", weight='bold', size='xl', color='#000000')
                 ]
             ),
             BoxComponent(
