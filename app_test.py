c40b94570dd032568a47b213715545aa40746a6a
mei <j6xul6ing@gmail.com>
2021-08-16 11:02:42 +0800

    test

--- app_test.py
+++ app_test.py
@@ -286,18 +286,7 @@ if __name__ == "__main__":
 
 def job1():
     CompanyMessage.PushMessage(line_bot_api)
-def job1_task():
-    threading.Thread(target=job1).start()
-schedule.every(30).seconds.do(job1_task)
-def job2():
-    if __name__ == "__main__":
-        app.run()
-def job2_task():
-    threading.Thread(target=job2).start()
-schedule.every(600).seconds.do(job2_task)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
 
 
 
